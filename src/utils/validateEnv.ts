8a42d43fc689be663575f72114f45033b0423c50
Rui <rui19981120.sns@gmail.com>
2023-07-02 15:25:25 +0900

    Ref
    translation

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -1,38 +1,30 @@
 const validateEnv = () => {
 	if (!process.env.BOT_TOKEN) {
-		console.warn("[x] worker > DiscordBotの起動に失敗しました。 \n => `process.env.BOT_TOKEN`が設定されていません。");
+		console.warn("[x] worker > DiscordBot failed to start. \n => `process.env.BOT_TOKEN` is not set.");
 		return false;
 	}
 	if (!process.env.CLIENT_ID) {
-		console.warn("[x] worker > DiscordBotのClientの特定に失敗しました。 \n => `process.env.CLIENT_ID`が設定されていません。");
+		console.warn("[x] worker > Failed to identify DiscordBot's Client. \n => `process.env.CLIENT_ID` is not set.");
 		return false;
 	}
 	if (!process.env.GUILD_ID) {
 		console.warn(
-			"[x] worker > DiscordBotが参加しているGuildを特定できませんでした。 \n => `process.env.GUILD_ID`が設定されていません。"
+			"[x] worker > Could not identify the Guild in which DiscordBot is participating. \n => `process.env.GUILD_ID` is not set."
 		);
 		return false;
 	}
 	if (!process.env.API_ENDPOINT_DEV || !process.env.API_ENDPOINT_PRO) {
 		console.warn(
-			"[x] worker > SEVENDAOのAPIのEndPointにアクセスできませんでした。 \n => `process.env.API_ENDPOINT_DEV` 又は `process.env.API_ENDPOINT_PRO`が設定されていません。"
-		);
-		return false;
-	}
-	if (!process.env.SEVENDAO_API_KEY) {
-		console.warn(
-			"[x] worker > SEVENDAOのAPIにアクセスできませんでした。 \n => `process.env.SEVENDAO_API_KEY`が設定されていません。"
+			"[x] worker > Could not access API EndPoint. \n => `process.env.API_ENDPOINT_DEV` or `process.env.API_ENDPOINT_PRO` is not set."
 		);
 		return false;
 	}
 	if (!process.env.MONGODB_URI) {
-		console.warn("[x] web > MongoDBに接続できませんでした。 \n => `process.env.MONGODB_URI`が設定されていません。");
+		console.warn("[x] web > Could not connect to MongoDB. \n => `process.env.MONGODB_URI` is not set.");
 		return false;
 	}
 	if (!process.env.ROOT_URL_DEV || !process.env.ROOT_URL_PRO) {
-		console.warn(
-			"[x] web > Fetchに失敗しました。 \n => `process.env.ROOT_URL_DEV` 又は `process.env.ROOT_URL_PRO` が設定されていません。"
-		);
+		console.warn("[x] web > Fetch failed. \n => `process.env.ROOT_URL_DEV` or `process.env.ROOT_URL_PRO` is not set.");
 		return false;
 	}
 	return true;
